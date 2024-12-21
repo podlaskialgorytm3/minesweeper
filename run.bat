0ad5860eb481e032565a96d3493bac3271a78548
podlaskialgorytm <live.m851@gmail.com>
2024-12-21 14:31:12 +0100

    first commit

---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Create directories build/executable and build/object_files if they do not exist
+if not exist build\object-files mkdir build\object-files
+
+rem Compile the .c source files to .o object files
+gcc -c src\main.c -o build\object-files\main.o
+rem Link object files into an executable
+gcc build\object-files\main.o -o program.exe
+
+rem End of script
